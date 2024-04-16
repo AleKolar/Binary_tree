e14ce28547377d20cf55151ca0b15abc6a8a2cfc
AleKolar <alekolar17982@gmail.com>
2024-04-16 19:06:12 +0300

    try_by_LinerSearch

--- main.py
+++ main.py
@@ -30,6 +30,15 @@ class BinaryTree:
             self.right_child = new_child
         return self
 
+    def pre_order(self):
+        print(self.value)  # процедура обработки
+
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.pre_order()  # рекурсивно вызываем функцию
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.pre_order()  # рекурсивно вызываем функцию
+
     def post_order(self):
         if self.left_child is not None:  # если левый потомок существует
             self.left_child.post_order()  # рекурсивно вызываем функцию
@@ -38,6 +47,15 @@ class BinaryTree:
             self.right_child.post_order()  # рекурсивно вызываем функцию
 
         print(self.value)  # процедура обработки
+
+    def in_order(self):
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.in_order()  # рекурсивно вызываем функцию
+
+        print(self.value)  # процедура обработки
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.in_order()  # рекурсивно вызываем функцию
 
 
 
@@ -51,9 +69,11 @@ node_5 = node_root.right_child.insert_right(9)
 # левое поддерево предыдущего узла корня /4|9|\
 node_9 = node_5.right_child.insert_left(4)
 
-node_root.post_order()
+#node_root.post_order()
+#node_root.in_order()
+node_root.pre_order()
 
 
-# 2, 5, 11,6, 7, 4, 9, 5, 2
+# post_order: 2, 5, 11, 6, 7, 4, 9, 5, 2
 
-
+# in_order: 2, 7, 5, 6, 11, 2, 5, 4, 9
