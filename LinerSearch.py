0b237bb890e6d82230b56c3cdcd6cc37b6236719
AleKolar <alekolar17982@gmail.com>
2024-04-18 20:05:24 +0300

    try_resolve_Stepik_tasks

--- LinerSearch.py
+++ LinerSearch.py
@@ -60,3 +60,24 @@ array = [i for i in range(1, 100)]  # 1,2,3,4,...
 
 # запускаем алгоритм на левой и правой границе
 print(f'Index of number {element}:', binary_search(array, element, 0, 99))
+
+'''Сортировка пузырьком'''
+
+array = [2, 3, 1, 4, 6, 5, 9, 8, 7]
+
+for i in range(len(array)):
+    for j in range(len(array) - i - 1):
+        if array[j] > array[j + 1]:
+            array[j], array[j + 1] = array[j + 1], array[j]
+
+print(array)
+
+'''Cортировки вставками'''
+
+for i in range(1, len(array)):
+    x = array[i]
+    idx = i
+    while idx > 0 and array[idx-1] > x:
+        array[idx] = array[idx-1]
+        idx -= 1
+    array[idx] = x
